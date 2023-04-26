b2456c1b1067bc57c8bd4a3778da4f4c02e60711
Albin Dalbert <albin.dalbert@protonmail.com>
2023-04-26 23:57:27 +0200

    extended tokens

--- src/token.rs
+++ src/token.rs
@@ -1,15 +1,65 @@
-pub enum Token {
-    TopLevel(TopLevel),
-    Word(String),
-    Number(i64),
-    Punctuation(char),
+pub struct Token {
+    kind: TokenKind,
+    len: u32,
 }
 
-pub enum TopLevel {
+pub enum TokenKind {
+    Atomic { kind: Atomic },
+    Compound { kind: Compound },
+}
+
+pub enum Compound {
     Cluster,
+    ClusterEnd,
+
     Enum,
+    EnumEnd,
+
     Struct,
+    StructEnd,
+
     Function,
+    FunctionEnd,
 }
 
-pub enum TokenKind {}
+pub enum Atomic {
+    LineComment, // #
+    WhiteSpace,
+    Literal { kind: LiteralKind },
+    // seperation characters
+    SemiCol, // ;
+    Comma,   // ,
+    Dot,     // .
+
+    OpenParen,  // (
+    CloseParen, // )
+
+    OpenBrace,  // {
+    CloseBrace, // }
+
+    OpenBracket,  // [
+    CloseBracket, // ]
+
+    And, // &
+    Or,  // |
+
+    // oporation characters
+    Equal,     // =
+    Bang,      // !
+    LowThen,   // <
+    GreatThen, // >
+    Subtract,  // -
+    Add,       // +
+    Star,      // *
+    Slash,     // /
+
+    Unknown, // Unknown token kind, error
+
+    Eof, // end of file
+}
+
+pub enum LiteralKind {
+    Int,
+    Float,
+    Char,
+}
